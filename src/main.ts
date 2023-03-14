c86dd29c5e7a8f8ea664057405837cc48fd1910b
Khalid <shkhalid95@mail.ru>
2023-03-14 14:39:50 +0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { NestFactory } from '@nestjs/core';
 import { ManualConfigEnum } from './configs/manual.config';
 import { PrismaService } from './prisma/prisma.service';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   const configService = app.get(ConfigService);
   const port = +configService.get(ManualConfigEnum.PORT);
